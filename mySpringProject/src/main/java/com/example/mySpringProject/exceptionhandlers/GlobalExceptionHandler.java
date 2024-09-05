3d1ffe21a949f3a5d9077e2a0bdf81332d16e8ea
enoch-sarkodie-07 <166805083+enoch-sarkodie-07@users.noreply.github.com>
2024-09-05 15:56:08 +0000

    refactor: completed the global error handler class to ensure exact error messages

--- mySpringProject/src/main/java/com/example/mySpringProject/exceptionhandlers/GlobalExceptionHandler.java
+++ mySpringProject/src/main/java/com/example/mySpringProject/exceptionhandlers/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.example.mySpringProject.exceptionhandlers;
 
 
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +12,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
  @ExceptionHandler(ProjectException.class)
  public ResponseEntity<Object> handleGlobalException(ProjectException projectException) {
-    return null;
+     ErrorResponse response = projectException.getResponse();
+     var cause = projectException.getCause();
+     HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+     switch (response){
+         case EMAIL_ALREADY_EXIST -> status = HttpStatus.CONFLICT;
+     }
+
+     ProjectExceptionPayload projectExceptionPayload = new ProjectExceptionPayload(
+             response.label,
+             status,
+             cause
+     );
+
+    return new ResponseEntity<>(projectExceptionPayload,status);
  }
 }
