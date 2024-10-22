c3f076e68fa56855ef1188cf7880c257ac782998
enoch Sarkodie <enochsarkodie07@gmail.com>
2024-10-22 21:21:27 +0000

    testing: started unit testing using JUnit5

--- mySpringProject/src/test/java/com/example/mySpringProject/service/AuthenticationServiceTest.java
+++ mySpringProject/src/test/java/com/example/mySpringProject/service/AuthenticationServiceTest.java
@@ -1,11 +1,17 @@
 package com.example.mySpringProject.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 class AuthenticationServiceTest {
+    @BeforeEach
+    void setUp(){
+        System.out.println("this supposed to execute before everything else in this class, yes cos I said so!!");
+    }
 
-   @Test
-    void registrationTesting (){
-
+    @Test
+    public void methodTest1(){
+        System.out.println("Checking out on some testing innit!!");
     }
 }
